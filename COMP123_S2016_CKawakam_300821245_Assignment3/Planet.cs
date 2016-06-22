322a4f01313d0560405b6ab1b8b3825ed3ccde40
ChinatsuKawakami <ckawakam@my.centennialcollege.ca>
2016-06-21 20:28:37 -0400

    Added Planet abstruct class and added fields and public properties in it.

---
+++ COMP123_S2016_CKawakam_300821245_Assignment3/Planet.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace COMP123_S2016_CKawakam_300821245_Assignment3
+{
+    public abstract class Planet
+    {
+       //Private Fields+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        private double _diameter;//readonly
+        private double _mass;//readon;y
+        private int _moonCount;
+        private string _name;//readonly
+        private double _orbitalPeriod;
+        private int _ringCount;
+        private double _rotationPeriod;
+
+        //PUBLIC PROPETIES+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        public double Diameter//ReadOnly
+        {
+            get
+            {
+               return this._diameter;
+            }
+        }//close Diameter
+
+        public double Mass // ReadOnly
+        {
+            get
+            {
+                return this._mass;
+            }
+        }//close Mass
+
+        public int MoonCount
+        {
+            get
+            {
+                return this._moonCount;
+            }
+            set
+            {
+                this._moonCount = value;
+            }
+        }//close MoonCount
+
+        public string Name // ReadOnly
+        {
+            get
+            {
+                return this._name;
+            }
+        }//close Name
+
+        public double OrbitalPeriod
+        {
+            get
+            {
+                return this._orbitalPeriod;
+            }
+            set
+            {
+                this._orbitalPeriod = value;
+            }
+        }//close OrbitalPeriod
+
+        public int RingCount
+        {
+            get
+            {
+                return this._ringCount;
+            }
+            set
+            {
+                this._ringCount = value;
+            }
+
+        }//close RingCount
+
+        public double RotationPeriod
+        {
+            get
+            {
+                return this._rotationPeriod;
+            }
+            set
+            {
+                this._rotationPeriod = value;
+            }
+        }//close RorationPeriod
+
+
+    }
+}
--- COMP123_S2016_CKawakam_300821245_Assignment3/Program.cs
+++ COMP123_S2016_CKawakam_300821245_Assignment3/Program.cs
@@ -11,7 +11,7 @@ using System.Threading.Tasks;
  * Student ID:300821245
  * Description:This program calls instance variables from Planet(abstract class),GiantPlanet,and TerrestrialPlanet class
  * and print ToString of GiantPlanet,ToString of TerrestrialPlanet, interface of IHasRings,IHasMoons,and IHabitable.
- * version:0.0.2-Added PlanetClassDiagram
+ * version:0.0.3-Added Planet abstruct class and added fields and public properties in it.
  */
 namespace COMP123_S2016_CKawakam_300821245_Assignment3
 {
