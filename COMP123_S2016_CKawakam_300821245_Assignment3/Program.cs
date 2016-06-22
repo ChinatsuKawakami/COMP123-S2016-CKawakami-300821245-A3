b0def8636dc40a7a7d82aaa632491e0a9a053a68
ChinatsuKawakami <ckawakam@my.centennialcollege.ca>
2016-06-21 20:12:42 -0400

    initial commit

---
+++ COMP123_S2016_CKawakam_300821245_Assignment3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/**
+ * This is driver class
+ * Author:Chinatsu Kawakami
+ * Date:17th June 2016
+ * Date Modified:21th June 2016
+ * Student ID:300821245
+ * Description:This program calls instance variables from Planet(abstract class),GiantPlanet,and TerrestrialPlanet class
+ * and print ToString of GiantPlanet,ToString of TerrestrialPlanet, interface of IHasRings,IHasMoons,and IHabitable.
+ * version:0.0.1-initial commit
+ */
+namespace COMP123_S2016_CKawakam_300821245_Assignment3
+{
+    /**
+     *This is the driver class for our program,Assignment3
+     *
+     * @class Program
+     */
+    class Program
+    {
+        /**
+         * The Main method for our driver class program
+         * 
+         * @constructor:Main
+         * @param {string[]}args
+         */
+        static void Main(string[] args)
+        {
+        }
+    }
+}
